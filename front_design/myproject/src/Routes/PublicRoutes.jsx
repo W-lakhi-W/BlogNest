a7ddddbba9f82d9846312649b85ad8d0a8b4a2e3
Lakhi <91194202+W-lakhi-W@users.noreply.github.com>
2025-08-19 16:05:01 +0530

    Delete front_design/myproject directory

--- front_design/myproject/src/Routes/PublicRoutes.jsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-const PublicRoutes = ({ children }) => {
-  const isAuthenticated = !!localStorage.getItem("accessToken");
-
-  return isAuthenticated ? <Navigate to="/dashboard" /> : children;
-};
-
-export default PublicRoutes;
--- front_design/myproject/vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
-import scrollbar from "tailwind-scrollbar";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-});
